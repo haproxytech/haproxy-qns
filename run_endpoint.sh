71f0c5e72ab7a948ce381f733e302c1aa77ff653
Frédéric Lécaille <flecaille@haproxy.com>
2022-06-16 18:27:14 +0200

    Add v2 test.

--- run_endpoint.sh
+++ run_endpoint.sh
@@ -11,7 +11,7 @@ set -eu
 # - CLIENT_PARAMS contains user-supplied command line parameters
 
 case $TESTCASE in
-versionnegotiation|handshake|transfer|retry|resumption|http3|multiconnect|zerortt|chacha20|keyupdate)
+versionnegotiation|handshake|transfer|retry|resumption|http3|multiconnect|zerortt|chacha20|keyupdate|v2)
 	:
 ;;
 *)
